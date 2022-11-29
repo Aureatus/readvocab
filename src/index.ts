d93338fbb3dd6af562f18e87e7ca011d0ddaf50a
Alex Knott <alextknott@gmail.com>
2022-11-29 13:17:15 +0000

    Import wordsRouter and use as middleware on /words path

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import compression from "compression";
 import helmet from "helmet";
 import cors from "cors";
+import wordsRouter from "./routes/wordsRouter";
 
 const app = express();
 const port = 3000;
@@ -16,6 +17,8 @@ app.use(cors());
 app.use("/test", (_req, res) => {
   res.send("test");
 });
+
+app.use("/words", wordsRouter);
 
 app.use((_req, res) => {
   res.sendStatus(404);
