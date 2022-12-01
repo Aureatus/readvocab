79f748b137dd625d1ad8238d3fab8d8a8215bde8
Alex Knott <alextknott@gmail.com>
2022-12-01 23:02:23 +0000

    Use helper functions to get rare words and their definitions from uploaded pdf file, and return them.

--- src/controllers/words.ts
+++ src/controllers/words.ts
@@ -1,14 +1,27 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
+import findDefinitions from "../helpers/findDefinitions.js";
+import findRareWords from "../helpers/findRareWords.js";
+import getWordsAndDefinitions from "../helpers/getWordsAndDefinitions.js";
+import wordsFromPDF from "../helpers/wordsFromPDF.js";
+
+interface DefinitionObject {
+  word: string;
+  PoS: string;
+  definition: string;
+}
 
 const getWords = async (
   request: FastifyRequest,
   _reply: FastifyReply
-): Promise<string> => {
+): Promise<DefinitionObject[]> => {
   const files = await request.saveRequestFiles();
   const filePath = files[0]?.filepath;
   if (filePath === undefined) throw Error("File not found");
+  const words = await wordsFromPDF(filePath);
+  const rareWords = findRareWords(words, 20);
+  const rareWordDefinitions = await findDefinitions(rareWords);
 
-  return "This is a get request on the word route";
+  return getWordsAndDefinitions(rareWords, rareWordDefinitions);
 };
 
 export { getWords };
