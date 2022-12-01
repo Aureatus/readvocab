d73ac9320c1b72d2f887b728a80f67caf4c5f652
Alex Knott <alextknott@gmail.com>
2022-12-01 22:59:51 +0000

    Read file that is uploaded and temporarily save it to have work done on

--- src/controllers/words.ts
+++ src/controllers/words.ts
@@ -1,9 +1,13 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
 
 const getWords = async (
-  _request: FastifyRequest,
+  request: FastifyRequest,
   _reply: FastifyReply
 ): Promise<string> => {
+  const files = await request.saveRequestFiles();
+  const filePath = files[0]?.filepath;
+  if (filePath === undefined) throw Error("File not found");
+
   return "This is a get request on the word route";
 };
 
