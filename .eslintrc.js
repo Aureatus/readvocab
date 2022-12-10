4f30723c027394d87b87a3f04aba7542e9e23926
Alex Knott <alextknott@gmail.com>
2022-12-10 17:26:34 +0000

    Set up eslint and prettier, and lint + prettify all files

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  extends: [
+    "@react-native-community",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {},
+    },
+  ],
+};
