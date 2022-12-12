deb7d211e88b9ac682a737deca98883557d58ac4
Alex Knott <alextknott@gmail.com>
2022-12-12 00:51:06 +0000

    Create function that fetches rare words for given file from readvocab API

---
+++ src/library/helpers/getRareWords.ts
@@ -0,0 +1,28 @@
+import type { DefinitionWord } from "../../types/dataTypes";
+
+import { FileSystemUploadType, uploadAsync } from "expo-file-system";
+
+const getRareWords = async (
+  fileData: string | File
+): Promise<DefinitionWord[]> => {
+  const URL = "https://readvocab-backend-production.up.railway.app/words";
+  if (fileData instanceof File) {
+    const formData = new FormData();
+    formData.append("pdf", fileData);
+    const response = await fetch(URL, {
+      method: "POST",
+      body: formData,
+    });
+    const data = await response.json();
+    return data;
+  } else {
+    const response = await uploadAsync(URL, fileData, {
+      fieldName: "pdf",
+      httpMethod: "POST",
+      uploadType: FileSystemUploadType.MULTIPART,
+    });
+    const data = JSON.parse(response.body);
+    return data;
+  }
+};
+export default getRareWords;
