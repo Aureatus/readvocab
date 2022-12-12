18ab18aa824ee2979314257c1150904b4d69be21
Alex Knott <alextknott@gmail.com>
2022-12-12 13:39:07 +0000

    Remove HomeDataProps type as it's not needed

--- src/types/propTypes.ts
+++
@@ -1,8 +0,0 @@
-import type { Dispatch, SetStateAction } from "react";
-import type { DefinitionWord } from "./dataTypes";
-
-export type HomeDataProps = {
-  setWordData: Dispatch<SetStateAction<DefinitionWord[]>>;
-  setWordDataLoading: Dispatch<SetStateAction<boolean>>;
-  setWordDataError: Dispatch<SetStateAction<Error | undefined>>;
-};
