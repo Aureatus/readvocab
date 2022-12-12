23d6bf5301365fcacd91af3e1ffb753617e2624d
Alex Knott <alextknott@gmail.com>
2022-12-12 22:14:24 +0000

    Create WordList that displays wordData in a list

---
+++ src/components/screens/WordList.tsx
@@ -0,0 +1,52 @@
+import { useContext } from "react";
+import { View, Text, FlatList, ActivityIndicator } from "react-native";
+
+import type { DefinitionWord } from "../../types/dataTypes";
+
+import WordDataContext from "../../library/context/WordDataContext";
+import WordItem from "../WordItem";
+
+const WordList = () => {
+  const context = useContext(WordDataContext);
+  if (context === null)
+    return (
+      <View>
+        <Text>Unknown Error</Text>
+      </View>
+    );
+  const { wordData, wordDataLoading, wordDataError } = context;
+
+  const renderItem = ({ item }: { item: DefinitionWord }) => (
+    <WordItem
+      word={item.word}
+      definition={item.definition}
+      wordClass={item.wordClass}
+    />
+  );
+
+  if (wordDataLoading)
+    return (
+      <View>
+        <ActivityIndicator size={"large"} />
+      </View>
+    );
+
+  if (wordDataError)
+    return (
+      <View>
+        <Text>{wordDataError.message}</Text>
+      </View>
+    );
+
+  return (
+    <View>
+      <FlatList
+        data={wordData}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.word}
+      />
+    </View>
+  );
+};
+
+export default WordList;
