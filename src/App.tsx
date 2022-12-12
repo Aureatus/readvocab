7cfe3add9d9ec523864e989629dd5e705c38d1bd
Alex Knott <alextknott@gmail.com>
2022-12-12 22:16:06 +0000

    Replace Placeholder2 component with WordList

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
-import { StatusBar } from "expo-status-bar";
-import { Button, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { useState } from "react";
 
-import type {
-  Placeholder2Props,
-  RootStackParamList,
-} from "./types/navigationTypes";
+import type { RootStackParamList } from "./types/navigationTypes";
 import type { DefinitionWord } from "./types/dataTypes";
 
 import WordDataContext from "./library/context/WordDataContext";
 import screenOptions from "./library/helpers/navigation/screenOptions";
 import Home from "./components/screens/Home";
+import WordList from "./components/screens/WordList";
 
 const { Navigator, Screen } = createBottomTabNavigator<RootStackParamList>();
-
-const Placeholder2 = ({ navigation: { navigate } }: Placeholder2Props) => {
-  return (
-    <View>
-      <Text>Placeholder 2</Text>
-      <Button title="Go to Home" onPress={() => navigate("Home")} />
-      <StatusBar style="auto" />
-    </View>
-  );
-};
 
 export default function App() {
   const [wordData, setWordData] = useState<DefinitionWord[]>([]);
@@ -45,7 +31,7 @@ export default function App() {
       >
         <Navigator initialRouteName="Home" screenOptions={screenOptions}>
           <Screen name="Home" component={Home} />
-          <Screen name="Placeholder2" component={Placeholder2} />
+          <Screen name="WordList" component={WordList} />
         </Navigator>
       </WordDataContext.Provider>
     </NavigationContainer>
