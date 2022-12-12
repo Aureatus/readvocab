8d1607c46f466461aa613acb202f2c489c39cde8
Alex Knott <alextknott@gmail.com>
2022-12-12 13:48:41 +0000

    Replace Placeholder1 component with Home screen

--- src/App.tsx
+++ src/App.tsx
@@ -8,31 +8,14 @@ import {
 import { useState } from "react";
 import type { DefinitionWord } from "./types/dataTypes";
 
+import Home from "./components/screens/Home";
+
 type RootStackParamList = {
-  Placeholder1: undefined;
+  Home: undefined;
   Placeholder2: undefined;
 };
 
 const { Navigator, Screen } = createBottomTabNavigator<RootStackParamList>();
-
-type Placeholder1Props = BottomTabScreenProps<
-  RootStackParamList,
-  "Placeholder1"
->;
-const Placeholder1 = ({
-  navigation: { navigate },
-}: Placeholder1Props): JSX.Element => {
-  return (
-    <View>
-      <Text>Placeholder 1</Text>
-      <Button
-        title="Go to Placeholder2"
-        onPress={() => navigate("Placeholder2")}
-      />
-      <StatusBar style="auto" />
-    </View>
-  );
-};
 
 type Placeholder2Props = BottomTabScreenProps<
   RootStackParamList,
@@ -43,10 +26,7 @@ const Placeholder2 = ({ navigation: { navigate } }: Placeholder2Props) => {
   return (
     <View>
       <Text>Placeholder 2</Text>
-      <Button
-        title="Go to Placeholder1"
-        onPress={() => navigate("Placeholder1")}
-      />
+      <Button title="Go to Home" onPress={() => navigate("Home")} />
       <StatusBar style="auto" />
     </View>
   );
@@ -59,8 +39,8 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Navigator initialRouteName="Placeholder1">
-        <Screen name="Placeholder1" component={Placeholder1} />
+      <Navigator initialRouteName="Home">
+        <Screen name="Home" component={Home} />
         <Screen name="Placeholder2" component={Placeholder2} />
       </Navigator>
     </NavigationContainer>
