57c5447e76a8cdd1dbf4cf4f023b85ca5aedc807
Alex Knott <alextknott@gmail.com>
2022-12-22 00:41:10 +0000

    Add correct type to corpus parameter

--- src/helpers/findRareWords.ts
+++ src/helpers/findRareWords.ts
@@ -1,11 +1,11 @@
 import removeDuplicates from "../utils/removeDuplicates.js";
 
-import type { CorpusWord } from "../types.js";
+import type { corpusInstance, CorpusWord } from "../types.js";
 
 const findRareWords = (
   wordList: string[],
   numberOfWords: number,
-  corpus: any
+  corpus: corpusInstance
 ): CorpusWord[] => {
   const uniqueWords = removeDuplicates(wordList);
   const rareWordObjects = corpus.getMatchedWords(uniqueWords, numberOfWords);
