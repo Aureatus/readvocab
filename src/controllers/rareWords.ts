21d8182be94f740f6fe774e8e2adc2156833adf7
Alex Knott <alextknott@gmail.com>
2022-12-22 00:45:10 +0000

    Get corpus from fastify instance and pass it to findRareWords

--- src/controllers/rareWords.ts
+++ src/controllers/rareWords.ts
@@ -1,16 +1,19 @@
 import findRareWords from "../helpers/findRareWords.js";
 
-import type { FastifyReply, FastifyRequest } from "fastify";
-import type { CorpusWord } from "../types.js";
+import type { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import type { corpusInstance, CorpusWord } from "../types.js";
 
-const rareWords = async (
+async function rareWords(
+  this: FastifyInstance & { corpus?: corpusInstance },
   request: FastifyRequest<{ Body: string }>,
   _reply: FastifyReply
-): Promise<CorpusWord[]> => {
+): Promise<CorpusWord[]> {
+  const corpus = this.corpus;
+  if (corpus === undefined) throw Error("Corpus not found.");
   const words: string[] = JSON.parse(request.body);
-  const rareWords = findRareWords(words, 20);
+  const rareWords = findRareWords(words, 20, corpus);
 
   return rareWords;
-};
+}
 
 export { rareWords };
