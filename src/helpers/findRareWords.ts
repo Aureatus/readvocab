0b1a42f7ee28a628fa09261b7e0b57e1e5bb6475
Alex Knott <alextknott@gmail.com>
2022-12-22 00:00:13 +0000

    Refactor to take corpus as an argument, and use that rather than creating own corpus instance.

--- src/helpers/findRareWords.ts
+++ src/helpers/findRareWords.ts
@@ -1,31 +1,12 @@
-import { corpusObject } from "corpus-word-freq";
 import removeDuplicates from "../utils/removeDuplicates.js";
 
 import type { CorpusWord } from "../types.js";
 
 const findRareWords = (
   wordList: string[],
-  numberOfWords: number
+  numberOfWords: number,
+  corpus: any
 ): CorpusWord[] => {
-  const corpus = corpusObject([
-    "Prep",
-    "Neg",
-    "Num",
-    "NoP",
-    "NoP-",
-    "Lett",
-    "Int",
-    "Inf",
-    "Conj",
-    "Pron",
-    "Det",
-    "DetP",
-    "Gen",
-    "Ex",
-    "Uncl",
-    "Fore",
-  ]);
-
   const uniqueWords = removeDuplicates(wordList);
   const rareWordObjects = corpus.getMatchedWords(uniqueWords, numberOfWords);
   return rareWordObjects;
