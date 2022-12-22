0e675309ab26c803f68e6e1d57c0101653ae0187
Alex Knott <alextknott@gmail.com>
2022-12-22 01:23:41 +0000

    Remove unneeded typings leftover from decorator only corpus implementation

--- src/controllers/rareWords.ts
+++ src/controllers/rareWords.ts
@@ -1,15 +1,14 @@
 import findRareWords from "../helpers/findRareWords.js";
 
 import type { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
-import type { corpusInstance, CorpusWord } from "../types.js";
+import type { CorpusWord } from "../types.js";
 
 async function rareWords(
-  this: FastifyInstance & { corpus?: corpusInstance },
+  this: FastifyInstance,
   request: FastifyRequest<{ Body: string }>,
   _reply: FastifyReply
 ): Promise<CorpusWord[]> {
   const corpus = this.corpus;
-  if (corpus === undefined) throw Error("Corpus not found.");
   const words: string[] = JSON.parse(request.body);
   const rareWords = findRareWords(words, 20, corpus);
 
