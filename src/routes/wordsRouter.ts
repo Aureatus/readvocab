aa3f906135d66f39a5da660288c0b18511f7d523
Alex Knott <alextknott@gmail.com>
2022-12-23 15:42:45 +0000

    Add post route for words controller

--- src/routes/wordsRouter.ts
+++ src/routes/wordsRouter.ts
@@ -1,9 +1,11 @@
 import type { FastifyInstance } from "fastify";
 import { pdfToWords } from "../controllers/pdfToWords.js";
 import { rareWords } from "../controllers/rareWords.js";
+import { words } from "../controllers/words.js";
 import { wordsAndDefinitions } from "../controllers/wordsAndDefinitions.js";
 
 const wordsRouter = async (fastify: FastifyInstance): Promise<void> => {
+  fastify.post("/words", words);
   fastify.post("/wordsFromPDF", pdfToWords);
   fastify.post<{ Body: string }>("/rareWords", rareWords);
   fastify.post<{ Body: string }>("/wordsAndDefinitions", wordsAndDefinitions);
