d6edc78aaee9fdf7fc5a8e148468c8701fb6c652
Alex Knott <alextknott@gmail.com>
2023-01-04 12:36:26 +0000

    Remove db test route

--- src/index.ts
+++ src/index.ts
@@ -69,17 +69,6 @@ await app.register(corpus, {
 
 await app.register(wordsRouter);
 
-app.get("/dbtest", async (_req, reply) => {
-  // Or this.mongo.client.db('mydb').collection('users')
-  // @ts-expect-error
-  const pdfCollection = app.mongo.db.collection("pdfs");
-
-  // if the id is an ObjectId format, you need to create a new ObjectId
-  const data = await pdfCollection.findOne();
-
-  return await reply.send(data);
-});
-
 const start = async (): Promise<void> => {
   try {
     await app.listen({ host: "0.0.0.0", port });
