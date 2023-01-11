5136a58405cec6b8d916dd8977a0c152466bcda6
Alex Knott <alextknott@gmail.com>
2023-01-11 22:37:49 +0000

    Use signup controller on signup route

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,9 +1,10 @@
 import type { FastifyInstance } from "fastify";
 import login from "../controllers/auth/login.js";
+import signup from "../controllers/auth/signup.js";
 
 const authRouter = async (fastify: FastifyInstance): Promise<void> => {
   fastify.post("/login", login);
-  fastify.post("/signup", () => {});
+  fastify.post("/signup", signup);
 };
 
 export default authRouter;
