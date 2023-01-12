30a8498be09dd8a476ce9fb207f70bf052d0ae98
Alex Knott <alextknott@gmail.com>
2023-01-12 15:18:02 +0000

    Create test get route that returns user on successful authentication

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -5,6 +5,15 @@ import signup from "../controllers/auth/signup.js";
 const authRouter = async (fastify: FastifyInstance): Promise<void> => {
   fastify.post("/login", login);
   fastify.post("/signup", signup);
+  fastify.get(
+    "/",
+    {
+      onRequest: [fastify.authenticate],
+    },
+    async function (request, _reply) {
+      return request.user;
+    }
+  );
 };
 
 export default authRouter;
