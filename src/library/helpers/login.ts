86c5df176322628ee40f4d957fb5f5d3034f0a70
Alex Knott <alextknott@gmail.com>
2023-01-14 16:58:28 +0000

    Create login function that takes login info and state setters

---
+++ src/library/helpers/login.ts
@@ -0,0 +1,23 @@
+import type { Dispatch, SetStateAction } from "react";
+import postLogin from "./network/postLogin";
+
+const login = async (
+  email: string,
+  password: string,
+  setEmailError: Dispatch<SetStateAction<Error | null>>,
+  setPasswordError: Dispatch<SetStateAction<Error | null>>,
+  setUser: Dispatch<SetStateAction<string | null>>
+) => {
+  try {
+    setEmailError(null);
+    setPasswordError(null);
+    const jwt = await postLogin(email, password);
+    setUser(jwt);
+  } catch (err) {
+    if (!(err instanceof Error)) return;
+    if (err.message === "Account with email doesn't exist.") setEmailError(err);
+    if (err.message === "Incorrect password.") setPasswordError(err);
+  }
+};
+
+export default login;
