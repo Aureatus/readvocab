a15ff905c9b8d893c6e23dcca9be4d7b514aa262
Alex Knott <alextknott@gmail.com>
2023-01-14 16:27:00 +0000

    Create function that calls login route of API to get jwt for user details

---
+++ src/library/helpers/network/postLogin.ts
@@ -0,0 +1,25 @@
+import { API_URL } from "@env";
+
+const postLogin = async (email: string, password: string) => {
+  const url = `${API_URL}/auth/login`;
+  const searchParams = new URLSearchParams();
+  searchParams.append("email", email);
+  searchParams.append("password", password);
+  const response = await fetch(url, { body: searchParams, method: "POST" });
+
+  if (!response.ok) {
+    const responseText = await response.text();
+    let error;
+    try {
+      error = JSON.parse(responseText);
+    } catch (err) {
+      error = responseText;
+    }
+    const errorMessage = error.message ?? responseText;
+    throw Error(errorMessage);
+  }
+
+  return response;
+};
+
+export default postLogin;
