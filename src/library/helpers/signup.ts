ef1545d691fe27eb4f66c643d058184b4d68ce24
Alex Knott <alextknott@gmail.com>
2023-01-15 16:42:09 +0000

    Create function that handles setting of data + errors, and navigation, of signup.

---
+++ src/library/helpers/signup.ts
@@ -0,0 +1,32 @@
+import type { Dispatch, SetStateAction } from "react";
+import postSignup from "./network/postSignup";
+
+const signup = async (
+  email: string,
+  password: string,
+  confirmPassword: string,
+  setEmailError: Dispatch<SetStateAction<Error | null>>,
+  setPasswordError: Dispatch<SetStateAction<Error | null>>,
+  setConfirmPasswordError: Dispatch<SetStateAction<Error | null>>,
+  setOtherError: Dispatch<SetStateAction<Error | null>>,
+  setUser: Dispatch<SetStateAction<string | null>>,
+  goBack: () => void
+) => {
+  try {
+    setEmailError(null);
+    setPasswordError(null);
+    setConfirmPasswordError(null);
+    const jwt = await postSignup(email, password, confirmPassword);
+    setUser(jwt);
+    goBack();
+  } catch (err) {
+    if (!(err instanceof Error)) return;
+    if (err.message === "Account with that email already exists.")
+      return setEmailError(err);
+    if (err.message === "Password confirmation doesn't match.")
+      return setConfirmPasswordError(err);
+    else return setOtherError(err);
+  }
+};
+
+export default signup;
