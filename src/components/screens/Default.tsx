cf533aba91c5334b10a7a48e193239ec2e679aa4
Alex Knott <alextknott@gmail.com>
2023-01-17 21:22:03 +0000

    Get user from context and update savedWords state when user changes using getSavedWords

--- src/components/screens/Default.tsx
+++ src/components/screens/Default.tsx
@@ -1,5 +1,5 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import type { DefinitionWord } from "../../types/dataTypes";
 import type { TabParamList } from "../../types/navigationTypes";
@@ -7,12 +7,25 @@ import type { TabParamList } from "../../types/navigationTypes";
 import Home from "./Home";
 import WordList from "./WordList";
 import screenOptions from "../../library/helpers/navigation/screenOptions";
+import useUserContext from "../../library/hooks/useUserContext";
 import SavedWordsContext from "../../library/context/SavedWordsContext";
+import getSavedWords from "../../library/helpers/network/getSavedWords";
 
 const { Navigator, Screen } = createBottomTabNavigator<TabParamList>();
 
 const Default = () => {
+  const { user } = useUserContext();
   const [savedWords, setSavedWords] = useState<DefinitionWord[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      if (user === null) setSavedWords([]);
+      else {
+        const response = await getSavedWords(user);
+        setSavedWords(response);
+      }
+    })();
+  }, [user]);
 
   return (
     <SavedWordsContext.Provider value={{ savedWords, setSavedWords }}>
