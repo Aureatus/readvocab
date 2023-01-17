db72513b8749b624a8acf5732f8cbf4bb0fcb69c
Alex Knott <alextknott@gmail.com>
2023-01-17 21:18:46 +0000

    Create hook to access SavedWordsContext with

---
+++ src/library/hooks/useSavedWordsContext.ts
@@ -0,0 +1,12 @@
+import { useContext } from "react";
+import SavedWordsContext from "../context/SavedWordsContext";
+
+const useSavedWordsContext = () => {
+  const context = useContext(SavedWordsContext);
+  if (context === undefined)
+    throw Error("SavedWordsContext must be used with a context provider!");
+
+  return context;
+};
+
+export default useSavedWordsContext;
