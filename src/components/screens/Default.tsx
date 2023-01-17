1bab87de2c332809943cafbab09c04fb0295b74a
Alex Knott <alextknott@gmail.com>
2023-01-17 21:19:50 +0000

    Create savedWords state and wrap returned Navigator in a context provider for it.

--- src/components/screens/Default.tsx
+++ src/components/screens/Default.tsx
@@ -1,17 +1,26 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import screenOptions from "../../library/helpers/navigation/screenOptions";
+import { useState } from "react";
+
+import type { DefinitionWord } from "../../types/dataTypes";
 import type { TabParamList } from "../../types/navigationTypes";
+
 import Home from "./Home";
 import WordList from "./WordList";
+import screenOptions from "../../library/helpers/navigation/screenOptions";
+import SavedWordsContext from "../../library/context/SavedWordsContext";
 
 const { Navigator, Screen } = createBottomTabNavigator<TabParamList>();
 
 const Default = () => {
+  const [savedWords, setSavedWords] = useState<DefinitionWord[]>([]);
+
   return (
-    <Navigator initialRouteName="Home" screenOptions={screenOptions}>
-      <Screen name="Home" component={Home} />
-      <Screen name="WordList" component={WordList} />
-    </Navigator>
+    <SavedWordsContext.Provider value={{ savedWords, setSavedWords }}>
+      <Navigator initialRouteName="Home" screenOptions={screenOptions}>
+        <Screen name="Home" component={Home} />
+        <Screen name="WordList" component={WordList} />
+      </Navigator>
+    </SavedWordsContext.Provider>
   );
 };
 
