183c11f2438d2951755ec73406d818d7cf8cbe13
Alex Knott <alextknott@gmail.com>
2023-01-17 21:23:17 +0000

    Update passed savedWords with new saved word on successful saving of word

--- src/library/helpers/saveWord.ts
+++ src/library/helpers/saveWord.ts
@@ -1,14 +1,21 @@
+import type { Dispatch, SetStateAction } from "react";
 import Toast from "react-native-root-toast";
 import type { DefinitionWord } from "../../types/dataTypes";
 import postSaveWord from "./network/postSaveWord";
 
-const saveWord = async (user: string | null, word: DefinitionWord) => {
+const saveWord = async (
+  user: string | null,
+  word: DefinitionWord,
+  savedWords: DefinitionWord[],
+  setSavedWords: Dispatch<SetStateAction<DefinitionWord[]>>
+) => {
   try {
     if (user === null)
       return Toast.show("Please log in to save words!", {
         position: Toast.positions.TOP,
       });
     await postSaveWord(user, word);
+    setSavedWords([...savedWords, word]);
   } catch (err) {
     if (!(err instanceof Error)) return;
     Toast.show(err.message, { position: Toast.positions.TOP });
