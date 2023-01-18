5408c9191d57b23d2e259944ec5c865022022754
Alex Knott <alextknott@gmail.com>
2023-01-18 20:35:28 +0000

    Use Buttons from react-native-paper

--- src/components/Header/HeaderRight.tsx
+++ src/components/Header/HeaderRight.tsx
@@ -1,6 +1,6 @@
 import { View, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { Button } from "react-native";
+import { Button } from "react-native-paper";
 import type { HomeScreenNavigationProp } from "../../types/navigationTypes";
 import useUserContext from "../../library/hooks/useUserContext";
 
@@ -13,27 +13,33 @@ const HeaderRight = () => {
       {user ? (
         <View style={styles.container}>
           <Button
-            title="logout"
+            mode="contained"
             onPress={() => {
               setUser(null);
             }}
             color={"#f3213d"}
-          />
+          >
+            logout
+          </Button>
         </View>
       ) : (
         <View style={styles.container}>
           <Button
-            title="login"
+            mode="contained"
             onPress={() => {
               navigate("Login");
             }}
-          />
+          >
+            login
+          </Button>
           <Button
-            title="signup"
+            mode="contained"
             onPress={() => {
               navigate("Signup");
             }}
-          />
+          >
+            signup
+          </Button>
         </View>
       )}
     </View>
