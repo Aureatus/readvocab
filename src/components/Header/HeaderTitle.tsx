dfacfc175c2d4f324fb0af606336b999194e1552
Alex Knott <alextknott@gmail.com>
2023-01-21 10:25:56 +0000

    Use react-native-paper Text variant and remove styling font size

--- src/components/Header/HeaderTitle.tsx
+++ src/components/Header/HeaderTitle.tsx
@@ -1,11 +1,14 @@
-import { Text, View, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Text } from "react-native-paper";
 import { Entypo } from "@expo/vector-icons";
 
 const HeaderTitle = ({ color, size }: { color: string; size: number }) => {
   return (
     <View style={styles.container}>
       <Entypo name="book" size={size} color={color} />
-      <Text style={styles.title}>Readvocab</Text>
+      <Text variant="displayMedium" style={styles.title}>
+        Readvocab
+      </Text>
     </View>
   );
 };
@@ -17,7 +20,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   title: {
-    fontSize: 36,
     marginLeft: 14,
   },
 });
