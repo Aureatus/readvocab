4de8fbab2c979715a99378dc2ff3371416732d99
Alex Knott <alextknott@gmail.com>
2023-01-21 10:22:00 +0000

    Style Home screen, and show dialog when words are ready, rather than displaying nav button.

--- src/components/screens/Home.tsx
+++ src/components/screens/Home.tsx
@@ -1,5 +1,12 @@
-import { View, Text, StyleSheet, Dimensions } from "react-native";
-import { ProgressBar, Button } from "react-native-paper";
+import { View, StyleSheet, Dimensions } from "react-native";
+import {
+  ProgressBar,
+  Button,
+  Surface,
+  Text,
+  Dialog,
+  Portal,
+} from "react-native-paper";
 
 import type { HomeProps } from "../../types/navigationTypes";
 
@@ -7,6 +14,7 @@ import getFile from "../../library/helpers/getFile";
 import getWords from "../../library/helpers/getWords";
 import useWordDataContext from "../../library/hooks/useWordDataContext";
 import getRandomWords from "../../library/helpers/network/getRandomWords";
+import { useEffect, useState } from "react";
 
 const Home = ({ navigation: { navigate } }: HomeProps) => {
   const {
@@ -17,6 +25,14 @@ const Home = ({ navigation: { navigate } }: HomeProps) => {
     setWordDataError,
   } = useWordDataContext();
 
+  const [dialogVisible, setDialogVisible] = useState(false);
+
+  useEffect(() => {
+    if (wordData.length !== 0) {
+      setDialogVisible(true);
+    }
+  }, [wordData]);
+
   if (loading)
     return (
       <View style={styles.container}>
@@ -26,43 +42,72 @@ const Home = ({ navigation: { navigate } }: HomeProps) => {
     );
 
   return (
-    <View>
-      <Button
-        mode="contained"
-        onPress={() => {
-          (async () => {
-            try {
-              const file = await getFile();
-              if (file === undefined) return;
-
-              getWords(file, setWordData, setWordDataLoading, setWordDataError);
-            } catch (err) {
-              if (err instanceof Error) setWordDataError(err);
-            }
-          })();
-        }}
-      >
-        {wordData.length === 0 ? "Upload PDF" : "Upload another PDF"}
-      </Button>
-      <Button
-        mode="contained"
-        onPress={() => {
-          (async () => {
-            try {
-              getRandomWords(setWordData, setWordDataLoading, setWordDataError);
-            } catch (err) {
-              if (err instanceof Error) setWordDataError(err);
-            }
-          })();
-        }}
-      >
-        {"Random PDF"}
-      </Button>
-      {wordData.length !== 0 && (
-        <Button mode="contained" onPress={() => navigate("WordList")}>
-          {"Go to Words"}
+    <View style={styles.container}>
+      <Surface style={styles.surface}>
+        <Button
+          mode="contained"
+          onPress={() => {
+            (async () => {
+              try {
+                const file = await getFile();
+                if (file === undefined) return;
+                getWords(
+                  file,
+                  setWordData,
+                  setWordDataLoading,
+                  setWordDataError
+                );
+              } catch (err) {
+                if (err instanceof Error) setWordDataError(err);
+              }
+            })();
+          }}
+        >
+          {wordData.length === 0 ? "Upload a PDF" : "Upload another PDF"}
         </Button>
-      )}
+        <Text variant="bodyLarge" style={styles.orText}>
+          Or
+        </Text>
+        <Button
+          mode="contained"
+          onPress={() => {
+            (async () => {
+              try {
+                getRandomWords(
+                  setWordData,
+                  setWordDataLoading,
+                  setWordDataError
+                );
+              } catch (err) {
+                if (err instanceof Error) setWordDataError(err);
+              }
+            })();
+          }}
+        >
+          {"Get a random PDF"}
+        </Button>
+      </Surface>
+      <Portal>
+        <Dialog
+          visible={dialogVisible}
+          onDismiss={() => setDialogVisible(false)}
+          style={styles.dialog}
+        >
+          <Dialog.Content>
+            <Text variant="bodyMedium">Rare Words are ready!</Text>
+          </Dialog.Content>
+          <Dialog.Actions style={styles.dialogActions}>
+            <Button
+              onPress={() => {
+                setDialogVisible(false);
+                navigate("WordList");
+              }}
+            >
+              Go to words
+            </Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
     </View>
   );
 };
@@ -74,9 +119,30 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  surface: {
+    display: "flex",
+    justifyContent: "center",
+    padding: Dimensions.get("window").width / 6,
+
+    borderRadius: 20,
+  },
   loadingBar: {
     height: 20,
     width: Dimensions.get("window").width / 1.5,
+  },
+  orText: {
+    alignSelf: "center",
+    marginVertical: 5,
+  },
+  dialog: {
+    maxWidth: "50%",
+    maxHeight: "20%",
+    alignSelf: "center",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  dialogActions: {
+    alignSelf: "center",
   },
 });
 
