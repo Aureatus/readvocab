ef801815638a37358d4b3d19f47daa60a13dae54
Alex Knott <alextknott@gmail.com>
2023-02-07 09:51:04 +0000

    Create search get route that returns a pdf search result given a searchParam query

---
+++ apps/backend/src/routes/words/searchPDF.ts
@@ -0,0 +1,33 @@
+import type { FastifyInstance } from "fastify";
+import fluentSchemaObject from "fluent-json-schema";
+import Pdf from "../../models/pdf.js";
+import type { SearchGeneric } from "../../types.js";
+
+const fluentSchema = fluentSchemaObject.default;
+
+const searchPDF = async (fastify: FastifyInstance): Promise<void> => {
+  const QuerySchema = fluentSchema
+    .object()
+    .prop("searchTerm", fluentSchema.string().required());
+
+  fastify.get<SearchGeneric>(
+    "/search",
+    { schema: { querystring: QuerySchema } },
+    async function saveWord(request, reply) {
+      const { searchTerm } = request.query;
+
+      const searchResults = await Pdf.find(
+        {
+          $text: { $search: searchTerm },
+        },
+        "creator title _id"
+      ).limit(10);
+
+      // SEARCH NOW WORKS, RETURN ONLY THE ID FOR THE SEARCHED RESULTS.
+
+      return await reply.send(searchResults);
+    }
+  );
+};
+
+export default searchPDF;
