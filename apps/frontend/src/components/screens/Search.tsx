ed2a34028db7f4adf87bdde7dfd5951a4e4ee10e
Alex Knott <alextknott@gmail.com>
2023-02-07 10:09:14 +0000

    Create Search screen

---
+++ apps/frontend/src/components/screens/Search.tsx
@@ -0,0 +1,16 @@
+import { View, StyleSheet } from "react-native";
+
+const Search = () => {
+  return <View style={styles.container} />;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default Search;
