2245db16d1288f7f9835911c8020f28f8ef2e1ec
Alex Knott <alextknott@gmail.com>
2023-02-07 10:34:22 +0000

    Implement ability to search, and results be displayed.

--- apps/frontend/src/components/screens/Search.tsx
+++ apps/frontend/src/components/screens/Search.tsx
@@ -1,7 +1,47 @@
-import { View, StyleSheet } from "react-native";
-
+import { useEffect, useState } from "react";
+import { View, StyleSheet, FlatList } from "react-native";
+import { Searchbar, Text } from "react-native-paper";
+import getSearchedPDF from "../../library/helpers/network/getSearchedPDF";
+import type { SearchResult } from "../../types/dataTypes";
 const Search = () => {
-  return <View style={styles.container} />;
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
+
+  const renderItem = ({ item }: { item: SearchResult }) => {
+    return (
+      <View>
+        <Text>{item._id}</Text>
+        <Text>{item.title}</Text>
+        <Text>
+          {item.creator.length > 1
+            ? item.creator.map((e, index) =>
+                !(index === item.creator.length - 1) ? `${e} & ` : e
+              )
+            : item.creator}
+        </Text>
+      </View>
+    );
+  };
+
+  useEffect(() => {
+    if (searchQuery === "") return;
+    (async () => setSearchResults(await getSearchedPDF(searchQuery)))();
+  }, [searchQuery]);
+
+  return (
+    <View style={styles.container}>
+      <Searchbar
+        placeholder="Search PDFs"
+        value={searchQuery}
+        onChangeText={(query) => setSearchQuery(query)}
+      />
+      <FlatList
+        data={searchResults}
+        renderItem={renderItem}
+        keyExtractor={(item) => item._id}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
