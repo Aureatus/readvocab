0322b04255c95f9bb0c8ad28b77f7119d85e0d4b
Alex Knott <alextknott@gmail.com>
2023-02-07 10:29:05 +0000

    Create function that returns PDF search result

---
+++ apps/frontend/src/library/helpers/network/getSearchedPDF.ts
@@ -0,0 +1,27 @@
+import Constants from "expo-constants";
+
+const getSearchedPDF = async (searchQuery: string) => {
+  const apiUrl = Constants.expoConfig?.extra?.["apiUrl"];
+  const url = `${apiUrl}/words/search${
+    "?" + new URLSearchParams({ searchTerm: searchQuery })
+  }`;
+  const response = await fetch(url, {
+    method: "GET",
+  });
+
+  if (!response.ok) {
+    const responseText = await response.text();
+    let error;
+    try {
+      error = JSON.parse(responseText);
+    } catch (err) {
+      error = responseText;
+    }
+    const errorMessage = error.message ?? responseText;
+    throw Error(errorMessage);
+  }
+
+  return await response.json();
+};
+
+export default getSearchedPDF;
